0f34d186264248c9eee27618ca0a26850d54c68d
Despot Maksimovic <despotmaximovic@gmail.com>
2021-03-12 16:54:16 +0100

    test

--- script.js
+++ script.js
@@ -8,6 +8,27 @@ const question = document.getElementById('question');
 const btns = document.getElementById('buttons-wrapper');
 const start_form = document.getElementById("start-form");
 
+const button1 = document.getElementById("btn-1");
+const button2 = document.getElementById("btn-2");
+const button3 = document.getElementById("btn-3");
+const button4 = document.getElementById("btn-4");
+
+const shuffle = array => {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+};
+
+const replaceButtons = answers => {
+    button1
+};
+
 start_form.addEventListener('submit', e => {
     e.preventDefault();
     if (start_form[0].value) {
@@ -18,9 +39,8 @@ start_form.addEventListener('submit', e => {
         start_form[0].value = '';
         start_form[0].placeholder = 'Unesi svoju sifru';
     } else {
-        // console.log("true");
         start_form[0].placeholder = 'Moras upisati sifru!';
-    }
+    };
 });
 
 btns.addEventListener('submit', e => {
@@ -31,8 +51,8 @@ btns.addEventListener('submit', e => {
         start_form.toggleAttribute('hidden');
         btns.toggleAttribute('hidden');
     } else {
-        socket.emit('izbor', e.submitter.id);
-    }
+        socket.emit('izbor', e.submitter.textContent);
+    };
 });
 
 socket.on('log', message => console.log(message));
@@ -40,4 +60,6 @@ socket.on('log', message => console.log(message));
 socket.on('res', (message, recv_obj) => {
     text.innerHTML = message;
     question.innerHTML = recv_obj.prompt;
+    const answers = shuffle(recv_obj.answers);
+    replaceButtons(answers);
 });
